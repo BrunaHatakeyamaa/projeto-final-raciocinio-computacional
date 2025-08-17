92328d3c03afc2a18c3f531317c5efeb3ee2792a
Bruna Hatakeyama <bruna_hatake@hotmail.com>
2025-08-17 14:37:34 -0300

    implementação de repetição usando while

--- menuPrincipal.py
+++ menuPrincipal.py
@@ -7,40 +7,51 @@ def mostrar_opcoes():
   print('(9) Voltar ao menu principal.')
   print()
 
-print('----- MENU PRINCIPAL -----')
-print()
-print('(1) Gerenciar estudantes.')
-print('(2) Gerenciar professores.')
-print('(3) Gerenciar disciplinas.')
-print('(4) Gerenciar turmas.')
-print('(5) Gerenciar matrículas.')
-print('(9) Sair.')
-print()
-escolha_numero = input('Informe a opção desejada: ')
-print()
-
-if escolha_numero == "1": 
-  print('***** [ESTUDANTES] MENU DE OPERAÇÕES *****')
-  mostrar_opcoes()
-  escolha_estudantes = input('Informe a opção desejada:')
-elif escolha_numero == "2":
-  print('***** [PROFESSORES] MENU DE OPERAÇÕES *****')
-  mostrar_opcoes()
-  escolha_professores = input('Informe a opção desejada:')
-elif escolha_numero == "3":
-  print('***** [DISCIPLINAS] MENU DE OPERAÇÕES *****')
-  mostrar_opcoes()
-  escolha_disciplinas = input('Informe a opção desejada:')
-elif escolha_numero == "4":
-  print('***** [TURMAS] MENU DE OPERAÇÕES *****')
-  mostrar_opcoes()
-  escolha_turmas = input('Informe a opção desejada:')
-elif escolha_numero == "5":
-  print('***** [MATRÍCULAS] MENU DE OPERAÇÕES *****')
-  mostrar_opcoes()
-  escolha_matriculas = input('Informe a opção desejada:')
-elif escolha_numero != "9": 
-  print('Opção invalida.')
+escolha_numero = ""
+while escolha_numero != "9":
+  print('----- MENU PRINCIPAL -----')
+  print()
+  print('(1) Gerenciar estudantes.')
+  print('(2) Gerenciar professores.')
+  print('(3) Gerenciar disciplinas.')
+  print('(4) Gerenciar turmas.')
+  print('(5) Gerenciar matrículas.')
+  print('(9) Sair.')
+  print()
+  escolha_numero = input('Informe a opção desejada: ')
+  print()
+  if escolha_numero == "1": 
+    escolha_estudantes = ""
+    while escolha_estudantes != "9":
+      print('***** [ESTUDANTES] MENU DE OPERAÇÕES *****')
+      mostrar_opcoes()
+      escolha_estudantes = input('Informe a opção desejada:')
+  elif escolha_numero == "2":
+    escolha_professores = ""
+    while escolha_professores != "9": 
+      print('***** [PROFESSORES] MENU DE OPERAÇÕES *****')
+      mostrar_opcoes()
+      escolha_professores = input('Informe a opção desejada:')
+  elif escolha_numero == "3":
+    escolha_disciplinas = ""
+    while escolha_disciplinas != "9":
+      print('***** [DISCIPLINAS] MENU DE OPERAÇÕES *****')
+      mostrar_opcoes()
+      escolha_disciplinas = input('Informe a opção desejada:')
+  elif escolha_numero == "4":
+    escolha_turmas = ""
+    while escolha_turmas != "9":
+      print('***** [TURMAS] MENU DE OPERAÇÕES *****')
+      mostrar_opcoes()
+      escolha_turmas = input('Informe a opção desejada:')
+  elif escolha_numero == "5":
+    escolha_matriculas = ""
+    while escolha_matriculas != "9":
+      print('***** [MATRÍCULAS] MENU DE OPERAÇÕES *****')
+      mostrar_opcoes()
+      escolha_matriculas = input('Informe a opção desejada:')
+  elif escolha_numero != "9": 
+    print('Opção invalida.')
 
 print()
 print('===== ATUALIZAÇÃO =====')
