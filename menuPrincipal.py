14993a18378e00b3010ffeb860daaccbddda6024
Bruna Hatakeyama <bruna_hatake@hotmail.com>
2025-09-14 21:50:55 -0300

    Adicionei as opções de incluir e listar, usando for na lista na opção de estudante

--- menuPrincipal.py
+++ menuPrincipal.py
@@ -1,59 +1,71 @@
 def mostrar_opcoes():
-  print()
+  print() # Pular uma linha
   print('(1) Incluir.')
   print('(2) Listar.')
   print('(3) Atualizar.')
   print('(4) Excluir.')
   print('(9) Voltar ao menu principal.')
-  print()
+  print() # Pular uma linha
 
 escolha_numero = ""
+lista_de_estudantes = []
 while escolha_numero != "9":
   print('----- MENU PRINCIPAL -----')
-  print()
+  print() # Pular uma linha
   print('(1) Gerenciar estudantes.')
   print('(2) Gerenciar professores.')
   print('(3) Gerenciar disciplinas.')
   print('(4) Gerenciar turmas.')
   print('(5) Gerenciar matrículas.')
   print('(9) Sair.')
-  print()
+  print() # Pular uma linha
   escolha_numero = input('Informe a opção desejada: ')
-  print()
+  print() # Pular uma linha
   if escolha_numero == "1": 
-    escolha_estudantes = ""
+    escolha_estudantes = "" # Declaração de varíavel
     while escolha_estudantes != "9":
-      print('***** [ESTUDANTES] MENU DE OPERAÇÕES *****')
+      print('***** [ESTUDANTES] MENU DE OPERAÇÕES *****') 
       mostrar_opcoes()
-      escolha_estudantes = input('Informe a opção desejada:')
-  elif escolha_numero == "2":
-    escolha_professores = ""
-    while escolha_professores != "9": 
-      print('***** [PROFESSORES] MENU DE OPERAÇÕES *****')
-      mostrar_opcoes()
-      escolha_professores = input('Informe a opção desejada:')
+      escolha_estudantes = input('Informe a opção desejada: ')
+      print() # Pular linha
+      if escolha_estudantes == "1":
+        print('===== INCLUSÃO =====')
+        print()
+        nome = input('Informe o nome do estudante: ')
+        print() # Pular linha
+        lista_de_estudantes.append(nome)
+      elif escolha_estudantes == "2":
+        print('===== LISTAGEM =====')
+        print()
+        if len(lista_de_estudantes) == 0: #caso lista_de_estudantes esteja vazia 
+          print('Não há estudantes cadastrados.')
+        else: # Caso o lista de estudantes n esteja vazio
+          for estudante in lista_de_estudantes:
+            print(f' - {estudante}')
+        print() # Pular linha
+      elif escolha_estudantes == "3":
+        print('AREA ATUALIZAR EM DESENVOLVIMENTO')
+        print() # Pular linha
+      elif escolha_estudantes == "4":
+        print('AREA EXCLUIR EM DESENVOLVIMENTO')
+        print() # Pular linha
+
+  elif escolha_numero == "2": 
+    print('AREA DE PROFESSORES EM DESENVOLVIMENTO.')
+    print() # Pular a linha
   elif escolha_numero == "3":
-    escolha_disciplinas = ""
-    while escolha_disciplinas != "9":
-      print('***** [DISCIPLINAS] MENU DE OPERAÇÕES *****')
-      mostrar_opcoes()
-      escolha_disciplinas = input('Informe a opção desejada:')
+    print('AREA DE DISCIPLINAS EM DESENVOLVIMENTO.')
+    print() # Pular a linha
   elif escolha_numero == "4":
-    escolha_turmas = ""
-    while escolha_turmas != "9":
-      print('***** [TURMAS] MENU DE OPERAÇÕES *****')
-      mostrar_opcoes()
-      escolha_turmas = input('Informe a opção desejada:')
+    print('AREA DE TURMAS EM DESENVOLVIMENTO.')
+    print() # Pular a linha
   elif escolha_numero == "5":
-    escolha_matriculas = ""
-    while escolha_matriculas != "9":
-      print('***** [MATRÍCULAS] MENU DE OPERAÇÕES *****')
-      mostrar_opcoes()
-      escolha_matriculas = input('Informe a opção desejada:')
+    print('AREA DE MATRÍCULAS EM DESENVOLVIMENTO.')
+    print() # Pular a linha
   elif escolha_numero != "9": 
     print('Opção invalida.')
 
-print()
+print() # Pular uma linha
 print('===== ATUALIZAÇÃO =====')
-print()
+print() # Pular uma linha
 print('Finalizando aplicação...')
